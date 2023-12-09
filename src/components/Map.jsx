599941c2fda59dea14cd99cec2fcd9d94efe47bf
Hatice-Altun <van.altun@outlook.com>
2023-12-09 22:39:44 +0100

    Map Component Example

---
+++ src/components/Map.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Map() {
+  return (
+    <>
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2480.27916844296!2d5.041087975793809!3d51.563115571826!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c6be0895dc5ebd%3A0xf0000b796d9bb381!2sTilburg%20University!5e0!3m2!1snl!2snl!4v1702156372379!5m2!1snl!2snl"
+            width="100%" height="550" style={{border:0}}></iframe>
+    </>
+  )
+}
+
+export default Map
